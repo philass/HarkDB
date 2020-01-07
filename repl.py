83db473c51ef6f8372b781d0d7c30252a1140639
Philip Lassen <philiplassen@gmail.com>
2020-01-07 03:23:11 +0100

    deleting excess files

--- repl.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-from parser import command_parser
-
-user_input = input("harkdb> ")
-while (user_input != ".exit"):
-  command_parser(user_input)
-  user_input = input("harkdb> ")
