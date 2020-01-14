b6ce9b59b9dc833f5fc32af66375c8d0a21d4374
Philip Lassen <philiplassen@gmail.com>
2020-01-14 11:58:22 +0100

    Some logic added to representation to handle where statements

--- CodeGen/futGen.py
+++ CodeGen/futGen.py
@@ -14,18 +14,31 @@ def query_to_representation(tables, query):
   
   
 
+
+
 class Representation:
   def __init__(self, tables, query):
     (table, idxs) = query_to_representation(tables, query)
+    self._where = query.get_Where()
     self._table = table
     self._idxs = idxs
-
 
   def get_table(self):
     return self._table
 
   def get_idxs(self):
     return self._idxs
+
+  def where_rewrite(self):
+    where_eq_adjusted = self._where.replace("=", " == ")
+    where_and_adjusted = where_eq_adjusted.replace(" and ", " && ")
+    where_AND_adjusted = where_and_adjusted.replace(" AND ", " && ")
+    where_or_adjusted = where_AND_adjusted.replace(" or ", " || ")
+    where_OR_adjusted = where_or_adjusted.replace(" OR ", " || ")
+    where_clean = where_OR_adjusted
+    with_inds = [(h, where_clean.find(h) for h in table.get_schema()]
+
+
 
   def generateFuthark(self):
     """
@@ -44,5 +57,7 @@ class Representation:
     fut_file.write("")
     fut_file.write(select_from_header)
     fut_file.write(result)
+
+
 
 
