401631534704428b649b5e4ee744205b83554b6f
Philip Lassen <philiplassen@gmail.com>
2020-02-14 02:15:53 +0100

    Formatted and cleaned the code style for pep8

--- parser/query.py
+++ parser/query.py
@@ -11,71 +11,91 @@ Having condk.... condm
 
 """
 
-import sqlparse as sqp #Library for tokenizing SQL statement
+import sqlparse as sqp  # Library for tokenizing SQL statement
+
 
 def query_parse(query):
-  """
-  Parse Query into SQL Dictionary with key value pairs of 
-  SubClause : identifier List
-  """
-  clause_dic = {"select" : [], "from" : [], "where" : [],
-                "having" : [], "group by" : []}
-  statement = sqp.parse(query)[0] #Get Tokenized list of first SQL statement
-  f = lambda x : str(x.ttype)[11:21] != 'Whitespace'
-  s1_clean = list(filter(f, list(statement)))
-  keywords = clause_dic.keys() - "where" # Where is treated different by sqparse"
-  for i in range(len(list(s1_clean))):
-    val = s1_clean[i].value
-    if val in keywords:
-      clause_dic[val] = identifier_to_list(s1_clean[i+1])
-    elif val[:5] == "where":
-      clause_dic["where"] = val[5:]
-  print(clause_dic)
-  return clause_dic
+    """
+    Parse Query into SQL Dictionary with key value pairs of
+    SubClause : identifier List
+    """
+    clause_dic = {"select": [], "from": [], "where": [],
+                  "having": [], "group by": []}
+    # Get Tokenized list of first SQL statement
+    statement = sqp.parse(query)[0]
+    def func(arg):
+        return str(arg.ttype)[11:21] != 'Whitespace'
+
+    s1_clean = list(filter(func, list(statement)))
+    # Where is treated different by sqparse"
+    keywords = clause_dic.keys() - "where"
+    for i in range(len(list(s1_clean))):
+        val = s1_clean[i].value
+        if val in keywords:
+            clause_dic[val] = identifier_to_list(s1_clean[i+1])
+        elif val[:5] == "where":
+            clause_dic["where"] = val[5:]
+    print(clause_dic)
+    return clause_dic
 
 
 def identifier_to_list(sqp_token):
-  """
-  Convert IDENTIFIER List or Identifier into a list of columns
-  """
-  try:
-    tokens = list(sqp_token)
-    named_cols = filter(lambda x: x.ttype == None)
-    hack = list(map(lambda x: x.value, named_cols))
+    """
+    Convert IDENTIFIER List or Identifier into a list of columns
+    """
+    try:
+        named_cols = filter(lambda x: x.ttype is None)
+        hack = list(map(lambda x: x.value, named_cols))
+        return [x.strip() for x in hack[0].split(',')]
+
+    # Assumes failure means we are working with Identifier only
+    finally:
+        hack = list([sqp_token.value])
     return [x.strip() for x in hack[0].split(',')]
-
-  #Assumes failure means we are working with Identifier only
-  except:
-    hack = list([sqp_token.value])
-    return [x.strip() for x in hack[0].split(',')]
-
-
 
 
 class Query:
-  def __init__(self, query):
     """
-    Constructor taking a string Query for analysis
+    Class represents a Query
     """
-    clause_dictionary = query_parse(query)
-    self._Select = clause_dictionary["select"]
-    self._From = clause_dictionary["from"]
-    self._Where = clause_dictionary["where"]
-    self._Having = clause_dictionary["having"]
-    self._Group_by = clause_dictionary["group by"]
 
-  def get_Select(self):
-    return self._Select
+    def __init__(self, query):
+        """
+        Constructor taking a string Query for analysis
+        """
+        clause_dictionary = query_parse(query)
+        self._select = clause_dictionary["select"]
+        self._from = clause_dictionary["from"]
+        self._where = clause_dictionary["where"]
+        self._having = clause_dictionary["having"]
+        self._group_by = clause_dictionary["group by"]
 
-  def get_From(self):
-    return self._From
+    def get_select(self):
+        """
+        Get select statement
+        """
+        return self._select
 
-  def get_Where(self):
-    return self._Where
+    def get_from(self):
+        """
+        Get from statement
+        """
+        return self._from
 
-  def get_Having(self):
-    return self._Having
+    def get_where(self):
+        """
+        Get where statement
+        """
+        return self._where
 
-  def get_Group_by(self):
-    return self._Group_by
+    def get_having(self):
+        """
+        Get having statement
+        """
+        return self._having
 
+    def get_group_by(self):
+        """
+        Get group by
+        """
+        return self._group_by
