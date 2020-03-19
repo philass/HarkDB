23a76b92aa555bfd36edf747a8101012aed75dd0
Philip Lassen <philiplassen@gmail.com>
2020-03-19 20:27:44 +0100

    Working benchmark script for C/Python backends

--- benchmark/backend_bench.sh
+++ benchmark/backend_bench.sh
@@ -4,32 +4,33 @@
 # Script for running benchmarks of 
 # different Futhark backends
 # 
+# By Philip Lassen
 ##################################################
 
-DB_TYPE="[100][5]u32"
-GCOL_TYPE="i32"
-futhark dataset -g $DB_TYPE -g $GCOL_TYPE > dataset.data
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
-ls
 
+# Set parameters for futhark
+# dataset generation
+
+DB_TYPE="[100][20]u32"
+DB_BOUND="--u32-bounds=0:100"
+GCOL_TYPE="i32"
+GCOL_BOUND="--i32-bounds=0:2"
+SCOL_TYPE="[4]i32"
+SCOL_BOUND="--i32-bounds=3:19"
+TCOL_TYPE="[4]i32"
+TCOL_BOUND="--i32-bounds=0:4"
+
+# Print Commands as they run
+set -x
+
+# Create Dataset file for benchmarking
+
+futhark dataset $DB_BOUND -g $DB_TYPE $GCOL_BOUND \
+			  -g $GCOL_TYPE $SCOL_BOUND \
+                          -g $SCOL_TYPE $TCOL_BOUND \
+			  -g $TCOL_TYPE > ../futhark/dataset.data
+
+ls ../futhark/       # For Debugging purposes
+futhark bench --backend=pyopencl ../futhark/groupby.fut
 
 
