c6e6a8de69bc98c9e59472b1a9d5f2cf0d3c1058
Philip Lassen <philiplassen@gmail.com>
2020-03-19 18:35:08 +0100

    test linguist

--- benchmark/backend_bench.sh
+++ benchmark/backend_bench.sh
@@ -9,6 +9,27 @@
 DB_TYPE="[100][5]u32"
 GCOL_TYPE="i32"
 futhark dataset -g $DB_TYPE -g $GCOL_TYPE > dataset.data
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
+ls
 
 
 
