4a30e1092a06a4cbf31063330389be1dd2443fa7
Philip Lassen <philiplassen@gmail.com>
2020-03-29 00:28:22 +0100

    refactored main to launch and execute

--- db_gpu_load.cpp
+++ db_gpu_load.cpp
@@ -1,5 +1,8 @@
+#include <iostream>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
+#include "load_csv.h"
 
 extern "C" {
 
@@ -22,41 +25,44 @@ void free_context(struct context* cont) {
   futhark_context_config_free(cont->cfg);
 }
 
-int main() {
-  uint32_t db[] = { 1, 2, 3, 4, 3, 4, 5, 6 };
-  int32_t select_cols[] = {0, 2};
-
-
+std::vector<uint32_t> launchAndExecute(uint32_t* db, int* select_cols, int rows, int cols) {
   struct context cont;
   create_context(&cont); 
   struct futhark_context_config *cfg = cont.cfg;
   struct futhark_context *ctx = cont.ctx;
-
-/*  
-  struct futhark_context_config *cfg = futhark_context_config_new();
-  struct futhark_context *ctx = futhark_context_new(cfg);
-*/
   
-  struct futhark_u32_2d *db_arr = futhark_new_u32_2d(ctx, db, 2, 4);
-  struct futhark_i32_1d *y_arr = futhark_new_i32_1d(ctx, select_cols, 2);
+  struct futhark_u32_2d *db_arr = futhark_new_u32_2d(ctx, db, rows, cols);
+  struct futhark_i32_1d *y_arr = futhark_new_i32_1d(ctx, select_cols, sizeof(select_cols)/sizeof(select_cols[0]));
   struct futhark_u32_2d *result;
  
   
   futhark_entry_main(ctx, &result, db_arr, y_arr);
   futhark_context_sync(ctx);
  
-  // NEED TO FIX THIS SO THAT IT ALLOCATES BASED ON RESULT SIZE AND NOT JUST 4
-  uint32_t* data = (uint32_t *) malloc (sizeof(uint32_t) * 4);
-  futhark_values_u32_2d(ctx, result, data);
-  for (int i = 0; i < 4; i++) {
-    printf("Result: %d\n", data[i]);
-  }
+  int64_t* shape;
+  shape = futhark_shape_u32_2d(ctx, result);
+  int64_t num_rows = shape[0];
+  int64_t num_cols = shape[1];
+  int64_t num_tots = num_rows * num_cols;
+  
+  uint32_t* data = (uint32_t *) malloc (sizeof(uint32_t) * num_tots);
 
-  free(data);
   futhark_free_u32_2d(ctx, db_arr);
   futhark_free_i32_1d(ctx, y_arr);
   futhark_free_u32_2d(ctx,result);
   free_context(&cont);
-//  futhark_context_free(ctx);
-// futhark_context_config_free(cfg);
+  std::vector<uint32_t> values(data, data + num_tots);
+  free(data);
+  return values;
+
+}
+
+
+int main() {
+  uint32_t db[] = { 1, 2, 3, 4, 3, 4, 5, 6 };
+  int32_t select_cols[] = {0, 2};
+  std::vector<uint32_t> vals = launchAndExecute(db, select_cols, 2, 4); 
+  for (uint32_t v : vals) {
+	  std::cout << v << std::endl;
+  }
 }
