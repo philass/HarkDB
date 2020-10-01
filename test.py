5fd7c7ba2e2b873bcfa9da77aa46c0ad25f4d0d2
Philip Lassen <philiplassen@gmail.com>
2020-10-01 02:02:38 +0200

    CI (#8)
    * Add CI/setup scripts
    
    * Add moz-sql-parser to requirement.txt
    
    * Add pandas
    
    * set numpy version to fix pandas dependency conflict
    
    * Add data file for tests

--- test.py
+++ test.py
@@ -1,9 +1,9 @@
 from FutharkContext import FutharkContext
+import time
 
 fc = FutharkContext()
 fc.create_table('game_1', 'data.csv')
 #sel = fc.sql("select col1, col3 from game_1")
 gpby = fc.sql("select col1,  max(col3) from game_1 group by col1")
-#print(sel)
 print(gpby)
 
